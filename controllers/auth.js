7f69934d8cdb75a043705de3d32357746e843848
Loay Al-Mugharghar <almughargharloay@gmail.com>
2024-11-20 20:20:52 +0300

    Sign-up Functionality, Sign-in, Sign-out, Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,59 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcrypt")
+
+//Invoke
+
+const User = require("../models/user")
 
 router.get("/sign-up", (req, res) => {
   res.render("auth/sign-up.ejs")
+})
+
+router.post("/sign-up", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username })
+  if (userInDatabase) {
+    return res.send(`Username is taken`)
+  }
+  if (req.body.password !== req.body.confirmPassword) {
+    res.send(`password & confirm password doesn't match`)
+  }
+
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+  req.body.password = hashedPassword
+
+  const user = await User.create(req.body)
+  res.send(`Thanks for signing up ${user.username}`)
+})
+
+router.get("/sign-in", async (req, res) => {
+  res.render("auth/sign-in.ejs")
+})
+
+router.post("/sign-in", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username })
+  if (!userInDatabase) {
+    res.send(`Login failed, please try again.`)
+  }
+  const validPass = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+  )
+  if (!validPass) {
+    res.send(`Login Failed, please try again.`)
+  }
+
+  req.session.user = {
+    username: userInDatabase.username,
+    _id: userInDatabase._id,
+  }
+
+  res.redirect("/")
+})
+
+router.get("/sign-out", (req, res) => {
+  req.session.destroy()
+  res.redirect("/")
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ dotenv.config()
 const mongoose = require("mongoose")
 const methodOverride = require("method-override")
 const morgan = require("morgan")
+const session = require("express-session")
 
 mongoose.connect(process.env.MONGODB_URI)
 
@@ -15,16 +16,35 @@ mongoose.connection.on("connected", () => {
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride("_method"))
 app.use(morgan("dev"))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
 
 //Import Controller
 const authController = require("./controllers/auth")
+const isSignedIn = require("./middleware/is-signed-in.js")
+const passUserToView = require("./middleware/pass-user-to-view.js")
 
 //Invoke Controller
 app.use("/auth", authController)
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+// Add our custom middleware right after the session middleware
+app.use(passUserToView)
+
 // Landing Page
 app.get("/", (req, res) => {
-  res.render("index.ejs")
+  res.render("index.ejs", { user: req.session.user })
 })
 
 app.listen(3005, () => {
