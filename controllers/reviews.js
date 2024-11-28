5bc37fa8197c555bf98e75d29788ddae74cfb3ae
Loay Al-Mugharghar <almughargharloay@gmail.com>
2024-11-28 19:48:56 +0300

    review functions are completed

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -26,9 +26,7 @@ router.get("/new/:movieId", async (req, res) => {
 
 router.post("/new", async (req, res) => {
   req.body.owner = req.session.user._id
-  console.log(req.body.owner)
   req.body.movie = req.body.movieId
-  console.log(req.body.movie)
   await Reviews.create(req.body)
   res.redirect("/")
 })
@@ -37,13 +35,31 @@ router.get("/:reviewId", async (req, res) => {
   const foundReview = await Reviews.find({ _id: req.params.reviewId }).populate(
     "movie"
   )
-  console.log(foundReview)
   res.render("reviews/show.ejs", { review: foundReview })
 })
 
 router.get("/:reviewId/edit", async (req, res) => {
-  const foundReview = await Reviews.find({ _id: req.params.reviewId })
-  res.render("reviews/edit.ejs", { foundReview })
+  const foundReview = await Reviews.find({ _id: req.params.reviewId }).populate(
+    "movie"
+  )
+  res.render("reviews/edit.ejs", { review: foundReview })
+})
+
+router.put("/:reviewId", async (req, res) => {
+  try {
+    await Reviews.findByIdAndUpdate(req.params.reviewId, req.body).populate(
+      "movie"
+    )
+    res.redirect("/")
+  } catch (error) {
+    console.log(error)
+    res.redirect("/")
+  }
+})
+
+router.delete("/:reviewId", async (req, res) => {
+  await Reviews.findByIdAndDelete(req.params.reviewId)
+  res.redirect("/")
 })
 
 module.exports = router
